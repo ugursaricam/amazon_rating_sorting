2913fb5522b0020b7feb99f95a8f0b41ff667ce8
Ugur Saricam <105130977+ugursaricam@users.noreply.github.com>
2023-03-16 10:05:31 +0300

    Update amazon_rating_sorting.py

--- amazon_rating_sorting.py
+++ amazon_rating_sorting.py
@@ -1,39 +1,3 @@
-
-##############################################################
-# Business Problem
-##############################################################
-
-# One of the most important problems in e-commerce is the correct calculation of the points given to the products after sales.
-# The solution to this problem means providing greater customer satisfaction for the e-commerce site,
-# prominence of the product for the sellers and a seamless shopping experience for the buyers.
-# Another problem is the correct ordering of the comments given to the products.
-# Since misleading comments will directly affect the sale of the product, it will cause both financial loss and loss of customers.
-# In the solution of these 2 basic problems, e-commerce site and sellers will increase their sales,
-# while customers will complete their purchasing journey without any problems.
-
-# This data set, which includes Amazon product data, includes product categories and various metadata.
-# The product with the most comments in the electronics category has user ratings and comments.
-
-# Total Features: 12    Total Row: 4915    CSV File Size: 71.9 MB
-
-# Variables:
-# reviewerID: User ID
-# asin: Product ID
-# reviewerName: Username
-# helpful: Useful review rating
-# reviewText: Review
-# overall: Product rating
-# summary: Review summary
-# unixReviewTime: Review time
-# reviewTime: Review time Raw
-# day_diff: Number of days since review
-# helpful_yes: The number of times the review was found helpful
-# total_vote: Number of votes given to the review
-
-##############################################################
-# 1. Data Preparation
-##############################################################
-
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 import math
@@ -129,15 +93,6 @@ df.sort_values('score_average_rating',ascending=False).head(20)
 
 # wilson_lower_bound
 def wilson_lower_bound(up, down, confidence=0.95):
-    """
-        Wilson Lower Bound Score hesapla
-
-        - Bernoulli parametresi p için hesaplanacak güven aralığının alt sınırı WLB skoru olarak kabul edilir.
-        - Hesaplanacak skor ürün sıralaması için kullanılır.
-        - Not:
-        Eğer skorlar 1-5 arasıdaysa 1-3 negatif, 4-5 pozitif olarak işaretlenir ve bernoulli'ye uygun hale getirilebilir.
-        Bu beraberinde bazı problemleri de getirir. Bu sebeple bayesian average rating yapmak gerekir.
-    """
     '''
     Parameters
     ----------
